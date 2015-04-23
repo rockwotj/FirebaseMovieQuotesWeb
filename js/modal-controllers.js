2acedbf2f84619f2ea76971e9cb519753bc2159f
rockwotj <rockwotj@rose-hulman.edu>
2015-04-23 15:03:01 -0400

    Initial Starting code

--- js/modal-controllers.js
+++ js/modal-controllers.js
@@ -2,23 +2,11 @@
 (function() {
   var app = angular.module("modal-controllers", [ "ui.bootstrap"]);
 
-  app.controller("InsertQuoteModalCtrl", function ($modalInstance, $timeout, movieQuote) {
-    this.isNewQuote = movieQuote.get() == undefined;
-    this.moviequote = movieQuote.get() || {quote:"", movie:""};
+  app.controller("AddQuoteModalCtrl", function ($modalInstance, $timeout, movieQuote) {
+    this.moviequote = {quote:"", movie:""};
     var _this = this;
-    if (!this.isNewQuote) {
-      this.saveQuote = function() {
-       movieQuote.save(_this.moviequote);
-      };
-    } else {
-      this.saveQuote = function() {};
-    }
     this.insertQuote = function () {
-      if (_this.isNewQuote) {
        $modalInstance.close(_this.moviequote);
-      } else {
-       $modalInstance.close();
-      }
     };
 
     this.cancel = function () {
@@ -33,6 +21,24 @@
     });
   });
 
+  app.controller("UpdateQuoteModalCtrl", function ($modalInstance, $timeout, movieQuote) {
+    this.moviequote = movieQuote.get();
+    var _this = this;
+    this.saveQuote = function() {
+      movieQuote.save(_this.moviequote);
+    };
+
+    this.done = function () {
+       $modalInstance.dismiss("done");
+    };
+
+    $modalInstance.opened.then(function() {
+      $timeout(function() {
+        // Note the opened promise is still too early.  Added a 100mS delay to give Chrome time to put the DOM in place.
+        document.querySelector("#quote-input").focus();
+      }, 100);
+    });
+  });
 
 	app.controller("DeleteQuoteModalCtrl", function ($modalInstance, movieQuoteInModal) {
 	  this.deleteQuote = function () {
